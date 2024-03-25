38ef715518189fc2464ec8a6a0075b5f19c895fa
Egor <kr3nky123@gmail.com>
2024-03-26 00:53:00 +0300

    added board + figures

---
+++ src/components/CellComponents.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FC } from "react";
+
+interface CellProps {
+    cell: Cell
+}
+
+const CellComponent: FC<CellProps> = ({cell, }) => {
+    return ( 
+        <div className={['cell', cell.color].join(' ')}>
+
+            {cell.figure?.logo && <img src={cell.figure.logo} alt="figure" />}
+        </div>
+     );
+}
+ 
+export default CellComponent;
